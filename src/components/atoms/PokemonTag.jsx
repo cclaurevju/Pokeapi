833f2b1aee6cd3b497a226a46f0a4019ea6e3784
cclaurevju <Carlos.Claure@jala.university>
2025-05-30 18:13:09 -0400

    style(card): add styles to pokemon card

--- src/components/atoms/PokemonTag.jsx
+++ src/components/atoms/PokemonTag.jsx
@@ -5,8 +5,11 @@ export default function PokemonTagBox() {
   const icon = "icon";
   const tagName = "Tag";
   return (
-    <div className="pk-card-tag bg-color-type-grass">
-      <img className="pk-card-tag-img" src="src\assets\images\icons\bug.png" />
+    <div className="pk-card-tag color-type-grass">
+      <img
+        className="pk-card-tag-img"
+        src="src\assets\images\icons\grass.png"
+      />
       <div className="pk-card-tag-text">{tagName}</div>
     </div>
   );
--- src/components/organisms/PokemonCard.jsx
+++ src/components/organisms/PokemonCard.jsx
@@ -7,12 +7,24 @@ export default function PokemonCard() {
   return (
     <div className="pk-card-container">
       <div className="pk-card bg-color-type-grass">
-        <div className="pk-card-properties">
-          <PokemonCardInfo />
-          <PokemonTagList />
+        <div className="pk-card-mask">
+          <img
+            className="pk-card-pokeball-mask"
+            src="src\assets\images\masks\pokeball.png"
+          />
+          <img
+            className="pk-card-dots-mask"
+            src="src\assets\images\masks\dots-horizontal.png"
+          />
         </div>
-        <div className="pk-card-img-container">
-          <img className="pk-card-img" src="src\assets\pngwing.com (1).png" />
+        <div className="pk-card-content">
+          <div className="pk-card-properties">
+            <PokemonCardInfo />
+            <PokemonTagList />
+          </div>
+          <div className="pk-card-img-container">
+            <img className="pk-card-img" src="src\assets\pngwing.com (1).png" />
+          </div>
         </div>
       </div>
     </div>
