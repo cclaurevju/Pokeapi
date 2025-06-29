54b1382eed7838b3a2c511c9da32a4874badd745
cclaurevju <Carlos.Claure@jala.university>
2025-06-29 02:51:19 -0400

    feat(not-found): add page implementation

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -1,3 +1,14 @@
+import "./pages.css";
+import { useNavigate } from "react-router-dom";
+import Button from "../atoms/Button";
+
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="content not-found-page">
+      <img src="src/assets/images/404.png" alt="" />
+      <p className="page-not-found-text">Page not found</p>
+      <Button label="Back to home" onClick={() => navigate("/")} />
+    </div>
+  );
 }
