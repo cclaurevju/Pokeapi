9d392f4811796cf108c2d9c7bec0668f8c2dde78
cclaurevju <Carlos.Claure@jala.university>
2025-07-13 19:47:11 -0400

    refactor(game): change to reducer

--- src/components/molecules/GameInfoLayout.jsx
+++ src/components/molecules/GameInfoLayout.jsx
@@ -2,11 +2,12 @@ export default function GameInfoLayout({
   streak = 0,
   attempt = 0,
   pokemonName = null,
+  isPokemonHidden = true,
 }) {
   return (
     <div className="game-score-layout">
       <div>Streak: {streak}</div>
-      <div>{pokemonName ?? "???"}</div>
+      <div>{isPokemonHidden ? "???" : pokemonName}</div>
       <div>Atempt: {attempt}</div>
     </div>
   );
--- src/components/organisms/GameLayout.jsx
+++ src/components/organisms/GameLayout.jsx
@@ -1,64 +1,109 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import PokemonShadow from "../atoms/PokemonShadow";
 import GameButtonList from "../molecules/GameButtonList";
 import axiosInstance from "../../api/axiosInstance";
 import { getRandomIds, getRandomIndex } from "../../utils/random";
+import GameScoreLayout from "../molecules/GameInfoLayout";
 
 const MAX_OPTIONS = 4;
+const MAX_ATTEMPTS = 2;
+
+const getDefaultGame = () => {
+  return {
+    streak: 0,
+    attempt: 0,
+    pokemons: [],
+    selectedPokemon: null,
+    isPokemonHidden: true,
+  };
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-pokemon":
+      if (
+        !state.pokemons.some((p) => p.id == action.pokemon.id) &&
+        state.pokemons.length < MAX_OPTIONS
+      )
+        return { ...state, pokemons: [...state.pokemons, action.pokemon] };
+      else return state;
+
+    case "set-selected-pokemon":
+      return { ...state, selectedPokemon: action.pokemon };
+
+    case "win":
+      return { ...getDefaultGame(), streak: state.streak + 1 };
+
+    case "fail":
+      if (state.attempt == MAX_ATTEMPTS)
+        return reducer(state, { type: "lose" });
+      else return { ...state, attempt: state.attempt + 1 };
+
+    case "lose":
+      return getDefaultGame();
+
+    default:
+      console.error("Unknown action type: ", action.type);
+      break;
+  }
+  // if (action.type == "add-pokemon") {
+  //   if (
+  //     !state.pokemons.some((p) => p.id == action.pokemon.id) &&
+  //     state.pokemons.length < MAX_OPTIONS
+  //   )
+  //     return { ...state, pokemons: [...state.pokemons, action.pokemon] };
+  //   else return state;
+  // }
+  // if (action.type == "set-selected-pokemon") {
+  //   return { ...state, selectedPokemon: action.pokemon };
+  // }
+  // if (action.type == "win") {
+  //   return { ...getDefaultGame(), streak: state.streak + 1 };
+  // }
+  // if (action.type == "fail") {
+  //   if (state.attempt == MAX_ATTEMPTS) return reducer(state, { type: "lose" });
+  //   else return { ...state, attempt: state.attempt + 1 };
+  // }
+  // if (action.type == "lose") {
+  //   return getDefaultGame();
+  // }
+};
 
 export default function GameLayout() {
-  const [pokemons, setPokemons] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [isPokemonHidden, setIsPokemonHidden] = useState(true);
-  const [uniqueRandomIds, setUniqueRandomIds] = useState(() =>
-    getRandomIds(MAX_OPTIONS)
-  );
-  const [randomPokemonId, setRandomPokemonId] = useState(() =>
-    getRandomIndex(MAX_OPTIONS)
-  );
   const [shake, setShake] = useState(false);
+  const [game, dispatch] = useReducer(reducer, getDefaultGame());
 
   const fetchPokemonInfo = async (id) => {
     const response = await axiosInstance.get(`/pokemon-species/${id}`);
     const pokemonResponse = response.data;
-    setPokemons((prev) =>
-      prev.length < MAX_OPTIONS && !prev.some((p) => p.id == pokemonResponse.id)
-        ? [
-            ...prev,
-            {
-              id: pokemonResponse.id,
-              names: pokemonResponse.names,
-            },
-          ]
-        : prev
-    );
+    const mappedPokemon = {
+      id: pokemonResponse.id,
+      names: pokemonResponse.names,
+    };
+    dispatch({ type: "add-pokemon", pokemon: mappedPokemon });
   };
 
   const fetchSelectedPokemon = async (id) => {
     const response = await axiosInstance.get(`/pokemon/${id}`);
     const pokemonResponse = response.data;
-    setSelectedPokemon({
+    const mappedPokemon = {
       id: pokemonResponse.id,
       label: pokemonResponse.name,
       sprites: pokemonResponse.sprites,
-    });
+    };
+    dispatch({ type: "set-selected-pokemon", pokemon: mappedPokemon });
   };
 
   useEffect(() => {
-    if (pokemons.length == 0) {
+    if (game.pokemons.length == 0) {
+      const uniqueRandomIds = getRandomIds(MAX_OPTIONS);
       uniqueRandomIds.forEach((id) => {
         fetchPokemonInfo(id);
       });
-      fetchSelectedPokemon(uniqueRandomIds[randomPokemonId]);
+      fetchSelectedPokemon(uniqueRandomIds[getRandomIndex(MAX_OPTIONS)]);
     }
-  }, [pokemons]);
-
-  const handleReset = () => {
-    setUniqueRandomIds(() => getRandomIds(MAX_OPTIONS));
-    setRandomPokemonId(() => getRandomIndex(MAX_OPTIONS));
-    setPokemons([]);
-    setIsPokemonHidden(true);
-  };
+  }, [game.pokemons]);
 
   const triggerShake = () => {
     setShake(true);
@@ -66,26 +111,39 @@ export default function GameLayout() {
   };
 
   const handleSelection = (option) => {
-    if (option.id == selectedPokemon.id) {
+    if (option.id == game.selectedPokemon.id) {
       setIsPokemonHidden(false);
       setTimeout(() => {
-        handleReset();
+        dispatch({ type: "win" });
+        setIsPokemonHidden(true);
       }, 3000);
     } else {
+      dispatch({ type: "fail" });
       triggerShake();
     }
   };
 
   return (
     <div className="content game-layout">
-      {selectedPokemon ? (
-        <PokemonShadow
-          imageUrl={selectedPokemon.sprites.other.home.front_default}
-          isHidden={isPokemonHidden}
-          shake={shake}
-        />
+      {game.selectedPokemon ? (
+        <>
+          <GameScoreLayout
+            streak={game.streak}
+            attempt={game.attempt}
+            pokemonName={game.selectedPokemon.label}
+            isPokemonHidden={isPokemonHidden}
+          />
+          <PokemonShadow
+            imageUrl={game.selectedPokemon.sprites.other.home.front_default}
+            isHidden={isPokemonHidden}
+            shake={shake}
+          />
+        </>
       ) : null}
-      <GameButtonList options={pokemons} handleSelection={handleSelection} />
+      <GameButtonList
+        options={game.pokemons}
+        handleSelection={handleSelection}
+      />
     </div>
   );
 }
