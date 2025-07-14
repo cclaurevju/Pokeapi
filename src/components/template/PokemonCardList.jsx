da2d330cac0a0f9fc3a27ac0e267bc3fbfbdd76a
cclaurevju <Carlos.Claure@jala.university>
2025-07-14 00:23:19 -0400

    feat(card-list): add fetch of pokemons from api

--- src/components/template/PokemonCardList.jsx
+++ src/components/template/PokemonCardList.jsx
@@ -1,17 +1,92 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import PokemonCard from "../organisms/PokemonCard";
+import axiosInstance from "../../api/axiosInstance";
+import Button from "../atoms/Button";
+
+const QUANTITY_LIMIT = 25;
+
+const emptyState = {
+  next: null,
+  pokemons: [],
+  isVirtualScrollActive: true,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-pokemons": {
+      const combinedArrays = [...state.pokemons, ...action.pokemons];
+      const pokemons = Array.from(
+        new Map(combinedArrays.map((p) => [p.name, p])).values()
+      );
+      return {
+        ...state,
+        pokemons: pokemons,
+        next: action.next,
+      };
+    }
+
+    case "activate-fetch":
+      return {
+        ...state,
+        isVirtualScrollActive: true,
+      };
+
+    case "deactivate-fetch":
+      return {
+        ...state,
+        isVirtualScrollActive: false,
+      };
+
+    default:
+      console.error("Unknown action type: ", action.type);
+      return state;
+  }
+};
 
 function PokemonCardList() {
-  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3];
-  const [pokemons, setPokemons] = useState([])
+  const [listState, dispatch] = useReducer(reducer, emptyState);
+
+  const fetchPokemons = async () => {
+    if (!listState.next) {
+      const response = await axiosInstance(`/pokemon?limit=${QUANTITY_LIMIT}`);
+      const pokemons = response.data.results;
+      dispatch({
+        type: "add-pokemons",
+        pokemons: pokemons,
+        next: response.data.next,
+      });
+    } else {
+      const endpoint = listState.next.split("v2")[1];
+      const response = await axiosInstance(endpoint);
+      const pokemons = response.data.results;
+      dispatch({
+        type: "add-pokemons",
+        pokemons: pokemons,
+        next: response.data.next,
+      });
+    }
+    dispatch({ type: "deactivate-fetch" });
+  };
+
   useEffect(() => {
-    const api = 
-  }, [])
+    if (listState.isVirtualScrollActive) {
+      fetchPokemons();
+    }
+  }, [listState.isVirtualScrollActive]);
   return (
     <div className="container pk-card-list content">
-      {cards.map((card) => {
-        return <PokemonCard />;
+      {listState.pokemons.map((pokemon) => {
+        return <PokemonCard key={pokemon.name} />;
       })}
+      <div className="virtual-scroll">
+        <Button
+          color="secondary"
+          onClick={() => {
+            dispatch({ type: "activate-fetch" });
+          }}
+          label="Load more"
+        />
+      </div>
     </div>
   );
 }
