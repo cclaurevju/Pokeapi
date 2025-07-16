4daeca41d7f3c81ce2c4154cbfcccefcf9a07087
cclaurevju <Carlos.Claure@jala.university>
2025-07-16 16:53:44 -0400

    refactor: change router approach from declarative to data

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,55 @@ import "../src/components/organisms/organisms.css";
 import "../src/components/template/template.css";
 import "../src/components/pages/pages.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PokemonSearcher from "./components/pages/PokemonSearcher.jsx";
+import PokemonGamePage from "./components/pages/PokemonGamePage.jsx";
+import PokemonDetailsPage from "./components/pages/PokemonDetailsPage.jsx";
+import PokemonGenerationsPage from "./components/pages/PokemonGenerationsPage.jsx";
+import PokemonTypesPage from "./components/pages/PokemonTypesPage.jsx";
+import NotFoundPage from "./components/pages/NotFoundPage.jsx";
+import Layout from "./components/template/Layout.jsx";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <PokemonGamePage />,
+      },
+      {
+        path: "pokemons",
+        children: [
+          { index: true, element: <PokemonSearcher /> },
+          {
+            path: ":id",
+            element: <PokemonDetailsPage />,
+          },
+        ],
+      },
+      {
+        path: "generations",
+        element: <PokemonGenerationsPage />,
+      },
+      {
+        path: "types",
+        element: <PokemonTypesPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
