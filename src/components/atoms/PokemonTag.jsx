ac4f24fbfcf03c24d63246efc9f7571d11d41fca
cclaurevju <Carlos.Claure@jala.university>
2025-07-27 15:37:09 -0400

    refactor: improve pokemon tag component

--- src/components/atoms/PokemonTag.jsx
+++ src/components/atoms/PokemonTag.jsx
@@ -1,13 +1,24 @@
 import { capitalizeText } from "../../utils/capitalizeText";
 
-export default function PokemonTagBox({ tag = "" }) {
+export default function PokemonTagBox({
+  tag = "",
+  showLabel = true,
+  size = "sm",
+  isCircle = false,
+}) {
   return (
-    <div className={`pk-card-tag color-type-${tag}`}>
+    <div
+      className={`pk-card-tag color-type-${tag} pk-tag-${size} ${
+        isCircle ? "pk-card-circle" : ""
+      }`}
+    >
       <img
         className="pk-card-tag-img"
         src={`src\\assets\\images\\icons\\${tag}.png`}
       />
-      <div className="pk-card-tag-text">{capitalizeText(tag)}</div>
+      {showLabel ? (
+        <div className="pk-card-tag-text">{capitalizeText(tag)}</div>
+      ) : null}
     </div>
   );
 }
