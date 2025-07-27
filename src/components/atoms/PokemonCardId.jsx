f483862a639bca2d46fa5e469a051de4bea08129
cclaurevju <Carlos.Claure@jala.university>
2025-07-27 01:59:48 -0400

    refactor: change formatId to util

--- src/components/atoms/PokemonCardId.jsx
+++ src/components/atoms/PokemonCardId.jsx
@@ -1,4 +1,7 @@
+import { formatId } from "../../utils/formatId";
+
 export default function PokemonCardId({ color = "white", id = "NaN" }) {
-  const formatedId = id.toString().padStart(4, "0");
-  return <div className={`pk-card-id pk-card-id-${color}`}># {formatedId}</div>;
+  return (
+    <div className={`pk-card-id pk-card-id-${color}`}># {formatId(id)}</div>
+  );
 }
